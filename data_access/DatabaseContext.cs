08d5936784f076c0280fba9316dc2b7a12366aec
Jhovany Santacruz <jsantacruze@hotmail.com>
2023-06-29 16:59:44 -0500

    Jhovany

---
+++ data_access/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using domain_layer.Security;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access
+{
+    public class DatabaseContext : IdentityDbContext<SystemUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ domain_layer/Security/SystemUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain_layer.Security
+{
+    public class SystemUser : IdentityUser
+    {
+        public byte[]? ProfilePicture { get; set; }
+        public long? empleado_id { get; set; }
+        //[ForeignKey("empleado_id ")]
+        //public Empleado? Empleado { get; set; }
+    }
+}
